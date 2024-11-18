81d2580033285678fa122fdc5d0c2233506f6cb2
marl0nx <unbanbaum2@gmail.com>
2024-11-18 07:59:19 +0100

    Init

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Initialisierung des Scanners
+        Scanner scanner = new Scanner(System.in);
+
+        // Eingabeaufforderung
+        System.out.println("Bitte gebe eine Zahl ein, um die Fibonacci-Folge zu berechnen:");
+        System.out.print("> ");
+        // Wir benutzen double, da ab einer bestimmten Stelle die Zahlen zu groß für int sind.
+        int input = scanner.nextInt();
+
+        /*
+         * Berechnung der nächsten Zahl in der Fibonacci-Folge.
+         * Die nächste Zahl ist die Summe der beiden vorherigen Zahlen.
+         * a = 1, b = 1 → next = 1 + 1 = 2
+         * a = 1, b = 2 → next = 1 + 2 = 3
+         * ...
+         */
+        double number_a = 1, number_b = 1;
+        for (int i = 0; i < input; i++) {
+            System.out.println(number_a);
+            double next = number_a + number_b;
+            number_a = number_b;
+            number_b = next;
+        }
+
+    }
+}
